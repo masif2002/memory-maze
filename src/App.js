9946b72176e6a0e422aa1d882f96f5b5f96a7a7b
masif2002 <masif2002@outlook.com>
2023-04-19 12:27:59 +0530

    Created Cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import shuffle from './utils/shuffle';
+import Card from './components/Card'
 
 function App() {
+  const [cards, setCards] = useState(shuffle)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='grid'>
+        {cards.map((card) =>
+           <Card
+              key={card.id}
+              image={card.image}
+              selected={false}
+           />
+        )}
+      </div>
   );
 }
 
---
+++ src/utils/shuffle.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const shuffle = () => {
+  const cards = [
+    { image: 'assets/css.png'},
+    { image: 'assets/dart.png'},
+    { image: 'assets/firebase.png'},
+    { image: 'assets/flutter.png'},
+    { image: 'assets/go.png'},
+    { image: 'assets/html5.png'},
+    { image: 'assets/jquery.png'},
+    { image: 'assets/js.png'},
+    // { image: 'assets/jsx.png'},
+    // { image: 'assets/next.png'},
+    // { image: 'assets/node.png'},
+    // { image: 'assets/react.png'},
+    // { image: 'assets/rust.png'},
+    // { image: 'assets/sql.png'},
+    // { image: 'assets/stack.png'},
+    // { image: 'assets/ts.png'},
+  ]
+
+  const shuffledDeck = 
+  [...cards, ...cards].sort(() => 0.5 - Math.random()).map((card) => ({...card, id: Math.random()}) )
+
+  return shuffledDeck
+
+}
+
+export default shuffle
